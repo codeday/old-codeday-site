04940a8ff4727ee26f746710311b7bf0478ead60
Tyler Menezes <tylermenezes@gmail.com>
2014-08-27 09:17:54 -0700

    Add Laravel

--- navigation.php
+++
@@ -1,8 +0,0 @@
-<nav class="site-nav text-right row">
-	<ul>
-		<li><a href="register.php">Register</a></li>
-		<li><a href="showcase.php">Showcase</a></li>
-		<li><a href="sponsor.php">Sponsor</a></li>
-		<li><a href="press.php">Press</a></li>
-	</ul>
-</nav>
