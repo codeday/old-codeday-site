45b1dffca2dbe33f3413d532bbc4ea0ef0395925
TJ Horner <me@tjhorner.com>
2017-11-22 21:02:46 -0800

    challenge

---
+++ app/Http/Controllers/ChallengeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace CodeDay\Http\Controllers;
+
+use \CodeDay\Models;
+
+class ChallengeController extends Controller {
+    public function getChallenge() {
+        $password = \Request::input("password");
+        
+        if($password == \Config::get("challenge")) {
+            return response()->download("../resources/assets/challenge.zip");
+        } else {
+            return response("Unauthorized")->header("X-The-Secret-You-Seek", "?password=" . \Config::get("challenge"));
+        }
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -63,6 +63,8 @@ $routes = function() {
 
     \Route::controller('/phone', '\CodeDay\Http\Controllers\PhoneController');
 
+    \Route::get('/challenge', '\CodeDay\Http\Controllers\ChallengeController@getChallenge');
+
 
     // Event-based Routes
     \Route::bind('event', function($webname) {
