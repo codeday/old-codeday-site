ceb761f4f0f30346f92c2e08f272778f02ebefd9
Victor Lourng <hello@victorlourng.com>
2018-03-02 01:50:11 -0500

    tabs to spaces

--- resources/lang/emoji/global.php
+++ resources/lang/emoji/global.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-	'link'                  => '📍 :link ➕ 🖱 📍📍📍 🆙',
-	'attend-cta'            => '➕ ℹ<br />👩‍💻🌞 :citySpan',
-	'cities-cta'            => '🖱 📍📍📍',
-	'organize-cta'          => '❌📍❓ <a href=\':link\'>🆘❗️</a>',
-	'sponsors'              => '👩‍💻🌞 ➡️ 📺 <a href=\'https://srnd.org/\'>🎓🔍🎨</a>❗ 💘💘💘 🌐 ↘️',
+    'link'                  => '📍 :link ➕ 🖱 📍📍📍 🆙',
+    'attend-cta'            => '➕ ℹ<br />👩‍💻🌞 :citySpan',
+    'cities-cta'            => '🖱 📍📍📍',
+    'organize-cta'          => '❌📍❓ <a href=\':link\'>🆘❗️</a>',
+    'sponsors'              => '👩‍💻🌞 ➡️ 📺 <a href=\'https://srnd.org/\'>🎓🔍🎨</a>❗ 💘💘💘 🌐 ↘️',
 ];
