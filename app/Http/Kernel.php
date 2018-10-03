42331cb079ffc5f828a4e6180303444c25b8cf5c
Tyler Menezes <tylermenezes@gmail.com>
2018-10-03 11:58:20 -0700

    Was not previously using this and it causes problems now

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,6 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \CodeDay\Http\Middleware\VerifyCsrfToken::class,
         ],
 
         'api' => [
